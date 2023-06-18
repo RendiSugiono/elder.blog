b5cc9d3f8682379351ae52c1d7cfa4b18dde6138
RendiSugiono <89713693+RendiSugiono@users.noreply.github.com>
2023-06-18 19:09:41 +0700

    comit pertama

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import Link from "next/link";
+import styles from "./page.module.css";
+
+const Links = [
+  {
+    id: 1,
+    title: "Home",
+    url: "/",
+  },
+  {
+    id: 2,
+    title: "About",
+    url: "/about",
+  },
+  {
+    id: 3,
+    title: "Post",
+    url: "/post",
+  },
+  {
+    id: 4,
+    title: "Portfolio",
+    url: "/portfolio",
+  },
+  {
+    id: 5,
+    title: "Contact",
+    url: "/contact",
+  },
+  {
+    id: 6,
+    title: "Dashboard",
+    url: "/dashboard",
+  },
+];
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Elder Blog</div>
+      <div className={styles.links}>
+        {Links.map((link) => (
+          <Link key={link.id} href={link.url}>
+            {link.title}
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
