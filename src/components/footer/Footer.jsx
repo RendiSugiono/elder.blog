e9728d03ae7fd2ec1daf627b09e7beaff79d1d5d
RendiSugiono <89713693+RendiSugiono@users.noreply.github.com>
2023-06-19 06:10:32 +0700

    nambah navbar & footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,7 +1,18 @@
-import React from "react";
+import Image from "next/image";
+import styles from "./page.module.css";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.title}>©️2023 All right reserved</div>
+      <div className={styles.icon}>
+        <Image src="/1.png" alt="facebook" height={15} width={15} />
+        <Image src="/2.png" alt="instagram" height={15} width={15} />
+        <Image src="/3.png" alt="twiter" height={15} width={15} />
+        <Image src="/4.png" alt="yuotube" height={15} width={15} />
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
